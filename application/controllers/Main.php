223fb766779053e4efed531a6bcec322b84715c1
折影轻梦 <776194970@qq.com>
2018-10-07 23:59:44 +0800

    你爹我肝作业去了

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,6 +4,7 @@ class Main extends CI_Controller {
     public function __construct(){
         parent::__construct();
         header("Access-Control-Allow-Origin: *");
+        header('Content-type: application/json');
         $this->load->model('Model');
     }
 
